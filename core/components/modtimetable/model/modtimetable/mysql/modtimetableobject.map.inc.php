38a055974f6a7bfce7eed0346e23a4e9475aafe9
digitalpenguin <murray@digitalpenguin.hk>
2021-03-26 15:01:43 +0800

    Fixed phptype for description field

--- core/components/modtimetable/model/modtimetable/mysql/modtimetableobject.map.inc.php
+++ core/components/modtimetable/model/modtimetable/mysql/modtimetableobject.map.inc.php
@@ -27,7 +27,7 @@ $xpdo_meta_map['modTimetableObject']= array (
     'description' => 
     array (
       'dbtype' => 'text',
-      'phptype' => 'text',
+      'phptype' => 'string',
       'null' => false,
       'default' => '',
     ),
